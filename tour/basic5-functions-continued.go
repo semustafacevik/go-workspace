cddc7b81d0316e3f7b2d5559dd560e56d60c9a01
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-08-09 23:40:08 +0300

    feat(basic-tour): completed 5 "basic" sections

---
+++ tour/basic5-functions-continued.go
@@ -0,0 +1,21 @@
+package tour
+
+import "fmt"
+
+func addContinued(x, y int) int {
+	return x + y
+}
+
+/*
+	https://tour.golang.org/basics/5
+
+	Functions continued
+	When two or more consecutive named function parameters share a type, you can omit the type from all but the last.
+
+	In this example, we shortened
+
+		x int, y int	->		x, y int
+*/
+func FunctionsContinued() {
+	fmt.Println(addContinued(22, 44))
+}
