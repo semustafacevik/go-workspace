22d7d05b91021c509eebeb0263a7544d2ecd679c
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-08-11 18:14:22 +0300

    refactor(basic-tour): renamed filenames, edited comments

---
+++ tour/basic3_exported_names.go
@@ -0,0 +1,22 @@
+package tour
+
+import (
+	"fmt"
+	"math"
+)
+
+// https://tour.golang.org/basics/3
+// Exported names
+
+// In Go, a name is exported if it begins with a capital letter. For example,
+// Pizza is an exported name, as is Pi, which is exported from the math package.
+// pizza and pi do not start with a capital letter, so they are not exported.
+// When importing a package, you can refer only to its exported names.
+// Any "unexported" names are not accessible from outside the package.
+// Run the code. Notice the error message.
+// To fix the error, rename math.pi to math.Pi and try it again.
+func ExportedNames() {
+	// fmt.Println(math.pi) cannot refer to unexported name math.pi
+
+	fmt.Println(math.Pi)
+}
