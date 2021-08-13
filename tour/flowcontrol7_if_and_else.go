2e0f9eae9d2093ef2d752866a1ecbd206049ae87
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-08-13 23:59:16 +0300

    feat(flowcontrol-tour): completed "flowcontrol" sections about if

---
+++ tour/flowcontrol7_if_and_else.go
@@ -0,0 +1,31 @@
+package tour
+
+import (
+	"fmt"
+	"math"
+)
+
+func powWithElse(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+// https://tour.golang.org/flowcontrol/7
+// If and else
+
+// Variables declared inside an if short statement
+// are also available inside any of the else blocks.
+//
+// (Both calls to pow return their results before
+// the call to fmt.Println in main begins.)
+func IfAndElse() {
+	fmt.Println(
+		powWithElse(3, 2, 10),
+		powWithElse(3, 3, 20),
+	)
+}
