0468e03c7ae2871fedc1208dc8a36eb8dc97435c
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-08-18 22:36:16 +0300

    feat(moretypes-tour): completed "moretypes" sections about pointer & struct

---
+++ tour/moretypes4_struct_pointers.go
@@ -0,0 +1,23 @@
+package tour
+
+import "fmt"
+
+// https://tour.golang.org/moretypes/4
+// Pointers to structs
+
+// Struct fields can be accessed through
+// a struct pointer.
+//
+// To access the field X of a struct when
+// we have the struct pointer p we could
+// write (*p).X. However, that notation is
+// cumbersome, so the language permits us
+// instead to write just p.X, without the
+// explicit dereference.
+func StructPointers() {
+	v := Vertex{1, 2}
+	p := &v
+
+	p.X = 1e2
+	fmt.Println(v)
+}
