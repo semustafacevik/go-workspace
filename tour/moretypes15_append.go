bf529564d8cff5eb470d60834dda06185d0e21a3
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-08-20 23:45:29 +0300

    feat(moretypes-tour): completed 5 "moretypes" sections about slice

---
+++ tour/moretypes15_append.go
@@ -0,0 +1,50 @@
+package tour
+
+// https://tour.golang.org/moretypes/15
+// Appending to a slice
+
+// It is common to append new elements to
+// a slice, and so Go provides a built-in
+// append function. The documentation of
+// the built-in package describes append.
+// -> https://pkg.go.dev/builtin#append
+//
+//  func append(s []T, vs ...T) []T
+//
+// The first parameter s of append is a
+// slice of type T, and the rest are T
+// values to append to the slice.
+//
+// The resulting value of append is a slice
+// containing all the elements of the
+// original slice plus the provided values.
+//
+// If the backing array of s is too small
+// to fit all the given values a bigger
+// array will be allocated. The returned
+// slice will point to the newly allocated
+//  array.
+//
+// (To learn more about slices, read the
+// Slices: usage and internals article. ->
+// https://blog.golang.org/go-slices-usage-and-internals )
+func Append() {
+	var s []int
+	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// We can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+
+	// TODO: research on slice cap
+	s = append(s, 5)
+	printSlice(s)
+}
