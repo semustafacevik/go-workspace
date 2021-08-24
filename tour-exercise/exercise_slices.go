d05a4619effc0930d99fc4e0a04a00e2121cc361
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-08-24 19:57:12 +0300

    feat(tour-exercise): added solution of "slices" exercise

---
+++ tour-exercise/exercise_slices.go
@@ -0,0 +1,43 @@
+package tourexercise
+
+import "golang.org/x/tour/pic"
+
+// https://tour.golang.org/moretypes/18
+// Exercise: Slices
+
+func Pic(dx, dy int) [][]uint8 {
+	p := make([][]uint8, dy)
+
+	for y := range p {
+		p[y] = make([]uint8, dx)
+
+		// try another combination
+		// to see other images
+		for x := range p[y] {
+			p[y][x] = uint8(x)
+			// p[y][x] = uint8(y)
+			// p[y][x] = uint8(x + y)
+			// p[y][x] = uint8(y - x)
+
+			// p[y][x] = uint8((x + y) / 2)
+			// p[y][x] = uint8(x * y)
+			// p[y][x] = uint8(x ^ y)
+
+			// p[y][x] = uint8((x ^ y) * (x ^ y))
+			// p[y][x] = uint8(x*x + y*y)
+		}
+	}
+
+	return p
+}
+
+// Solution of "slices" exercise
+//
+// For detail:
+// https://tour.golang.org/moretypes/18
+//
+// For visual result:
+// https://play.golang.org/p/14yIpsPP65v
+func Slices() {
+	pic.Show(Pic)
+}
