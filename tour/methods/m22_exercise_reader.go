4005c52bb80028d626a071cd9f62dcfc8a26885b
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-09-02 16:55:54 +0300

    feat(methods-tour): completed "methods" sections about readers

---
+++ tour/methods/m22_exercise_reader.go
@@ -0,0 +1,12 @@
+package methods
+
+import "fmt"
+
+// https://tour.golang.org/methods/22
+// Exercise: Readers
+
+// Implement a Reader type that emits an
+// infinite stream of the ASCII character 'A'.
+func ExReader() {
+	fmt.Println("Please read and implement :)")
+}
