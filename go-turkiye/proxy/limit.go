75db6e8dc53b18ded56478c5e3cdda1b9773de94
Mustafa CEVIK <se.mustafacevik@gmail.com>
2021-10-09 14:02:41 +0300

    fix(proxy): fixed limit proxy controls

--- go-turkiye/proxy/limit.go
+++ go-turkiye/proxy/limit.go
@@ -32,23 +32,36 @@ func (p LimitProxy) Accept(key string) bool {
 func (p LimitProxy) Proxy(c *fiber.Ctx) error {
 	path := c.Path()
 
-	if v, ok := counter[path]; ok && v.count >= p.limit && v.ttl.After(time.Now()) {
-		c.Response().SetStatusCode(fiber.StatusTooManyRequests)
-		log.Printf("request limit reached for \"%s\"", path)
-		return fiber.ErrTooManyRequests
-	} else if (ok && v.ttl.Before(time.Now())) || !ok {
-		mutex.Lock()
-		counter[path] = &Limit{count: 0, ttl: time.Now().Add(p.ttl)}
-		mutex.Unlock()
+	if v, ok := counter[path]; ok && v.count >= p.limit {
+		if v.ttl.After(time.Now()) {
+			c.Response().SetStatusCode(fiber.StatusTooManyRequests)
+			log.Printf("request limit reached for \"%s\"", path)
+			return fiber.ErrTooManyRequests
+		} else {
+			log.Printf("resetting counter values of \"%s\"", path)
+			defineCounter(path, p.ttl)
+		}
+	} else if !ok {
+		defineCounter(path, p.ttl)
 	}
 
 	if err := c.SendString("go-workspace | proxy -> " + path); err != nil {
 		return err
 	}
 
+	incrementCounter(path)
+
+	return nil
+}
+
+func defineCounter(path string, ttl time.Duration) {
+	mutex.Lock()
+	counter[path] = &Limit{count: 0, ttl: time.Now().Add(ttl)}
+	mutex.Unlock()
+}
+
+func incrementCounter(path string) {
 	mutex.Lock()
 	counter[path].count++
 	mutex.Unlock()
-
-	return nil
 }
